35ba29b27a10219f92439969892b1b2aac08bda0
lech.gajewski <lech.gajewski@jamf.com>
2023-04-12 10:29:10 +0200

    Restore the v1 import - autoremoved but needed

--- kuberos.go
+++ kuberos.go
@@ -17,6 +17,7 @@ import (
 	"github.com/spf13/afero"
 	"go.uber.org/zap"
 	"golang.org/x/oauth2"
+	"k8s.io/api/core/v1"
 	"k8s.io/client-go/tools/clientcmd"
 	"k8s.io/client-go/tools/clientcmd/api"
 )
